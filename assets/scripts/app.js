85bb52b4e405c34d780fd38b7358fcc2e04eb30a
wostrau <a.ostrovtsov@gmail.com>
2023-09-25 13:03:34 +0200

    added functionality of fetching posts on main page

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,5 +1,22 @@
+const listElement = document.querySelector('.posts');
+const postTemplate = document.getElementById('single-post');
+
 const xhr = new XMLHttpRequest();
 
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/posts');
+
+xhr.responseType = 'json';
+
+xhr.onload = () => {
+    // const listIfPosts = JSON.parse(xhr.response);
+    const listIfPosts = xhr.response;
+
+    for (const post of listIfPosts) {
+        const postEl = document.importNode(postTemplate.content, true);
+        postEl.querySelector('h2').textContent = post.title.toUpperCase();
+        postEl.querySelector('p').textContent = post.body;
+        listElement.append(postEl);
+    }
+};
 
 xhr.send();
